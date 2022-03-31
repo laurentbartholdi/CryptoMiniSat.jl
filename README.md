656a6937c2585d6635349793e37078c4fefadb2d
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2022-03-30 22:42:28 -0700

    Finished the top-level interface

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # CryptoMiniSat
+
+This is a user interface, in Julia, to the [CryptoMiniSat|https://github.com/msoos/cryptominisat] SAT solver.
+
+It exposes all the functions of the C interface, in the same syntax (cmsat_new, etc.), and provides higher-level interfaces:
+- the same functions as in the C interface, without `cmsat_`, accept 1-based variables of any integer type (rather than the 0-based UInt32)
+- a high-level `solve(clauses; ...)` function that accepts a vector of vectors of clauses, represented as integers (negative for negated terms) for one-go solving, as well as an iterator `itersolve(clauses; ...)`
+- a higher-level `solve(clauses; ...)` in which the terms of the clauses are `Lit(x)`, `NotLit(x)` or generally `Lit(x,negated::Bool)` for usual clauses, and `x` may be of any type; or the terms are of the form `XorLit(x)` and `XNorLit(x)` for XOR-clauses, in which the number of non-satisfied terms should have the same parity as the number of XNorLit terms.
+
+```julia
+julia> using CryptoMiniSat
+julia> # simple clauses
+julia> cnf = [[1, -5, 4], [-1, 5, 3, 4], [-3, -4]];
+julia> solve(cnf)
+5-element Array{Int64,1}:
+ -1
+ -2
+ -3
+ -4
+ -5
+julia> # coffee with cream, tea with milk
+julia> cnf = [[XorLit(:coffee),XorLit(:tea)],[NotLit(:coffee),Lit(:cream)],[NotLit(:tea),Lit(:milk)]]
+julia> collect(itersolve(cnf))
+4-element Vector{Any}:
+ Dict{Symbol, Bool}(:cream => 0, :milk => 1, :tea => 1, :coffee => 0)
+ Dict{Symbol, Bool}(:cream => 1, :milk => 1, :tea => 1, :coffee => 0)
+ Dict{Symbol, Bool}(:cream => 1, :milk => 1, :tea => 0, :coffee => 1)
+ Dict{Symbol, Bool}(:cream => 1, :milk => 0, :tea => 0, :coffee => 1)
+```
+
+
--- src/CryptoMiniSat.jl
+++ src/CryptoMiniSat.jl
@@ -2,7 +2,7 @@ module CryptoMiniSat
 
 import CryptoMiniSat_jll
 
-export Lit, CMS, new_vars, nvars, add_clause, add_xor_clause, solve, get_model, get_conflict, itersolve
+export CMSLit, CMS, new_vars, nvars, add_clause, add_xor_clause, solve, get_model, get_conflict, itersolve, Lit, NotLit, XorLit, XNorLit
 
 import Base: show, convert, iterate, IteratorSize
 
@@ -11,6 +11,9 @@ const libcms = CryptoMiniSat_jll.libcryptominisat5
 const L_TRUE = 0x00
 const L_FALSE = 0x01
 const L_UNDEF = 0x02
+
+################################################################
+# low-level interface, CMSLit or 0-based
 
 # the main structure
 mutable struct CMS
@@ -21,10 +24,10 @@ convert(::Type{Ptr{Cvoid}}, p::CMS) = p.ptr
 
 creates a new CryptoMiniSat problem, with optional number of variables and threas."""
 function CMS(nvars::Int = 0; num_threads::Int = Sys.CPU_THREADS)
-    p = cms_new()
-    num_threads > 1 && cms_set_num_threads(p, num_threads)
-    nvars > 0 && cms_new_vars(p, nvars)
-    finalizer(cms_free, p)
+    p = cmsat_new()
+    num_threads > 1 && cmsat_set_num_threads(p, num_threads)
+    nvars > 0 && cmsat_new_vars(p, nvars)
+    finalizer(cmsat_free, p)
     p
 end
     
@@ -32,64 +35,77 @@ function Base.show(io::IO, p::CMS)
     if p.ptr == C_NULL
         print(io, "Unallocated CMS problem")
     else
-        print(io, cms_nvars(p), "-variable CMS problem")
+        print(io, cmsat_nvars(p), "-variable CMS problem")
     end
 end
 
 # literals
-struct Lit
+struct CMSLit
     x::UInt32
 end
-Lit(v::Int) = Lit(v > 0 ? UInt32(2v-2) : v < 0 ? UInt32(-2v-1) : error("0 is not a valid literal"))
-Lit(v::Int, invert::Bool) = Lit(UInt32(2v + (invert ? 1 : 0)))
-Base.convert(::Type{Lit}, x::Int) = Lit(x)
-Base.convert(::Type{Int}, x::Lit) = sign(x) ? -1-var(x) : 1+var(x)
-Base.convert(::Type{Int32}, x::Lit) = Int32(convert(Int, x))
-var(l::Lit) = Int(l.x >> 1)
-sign(l::Lit) = Bool(l.x & 0x01)
+CMSLit(v::Int) = CMSLit(v > 0 ? UInt32(2v-2) : v < 0 ? UInt32(-2v-1) : error("0 is not a valid literal"))
+CMSLit(v::Int, invert::Bool) = CMSLit(UInt32(2v + (invert ? 1 : 0)))
+Base.convert(::Type{CMSLit}, x::Int) = CMSLit(x)
+Base.convert(::Type{Int}, x::CMSLit) = sign(x) ? -1-var(x) : 1+var(x)
+Base.convert(::Type{Int32}, x::CMSLit) = Int32(convert(Int, x))
+var(l::CMSLit) = Int(l.x >> 1)
+sign(l::CMSLit) = Bool(l.x & 0x01)
 
-function Base.show(io::IO, l::Lit)
-    print(io, "Lit(",var(l),",",sign(l),")")
+function Base.show(io::IO, l::CMSLit)
+    print(io, "CMSLit(",var(l),",",sign(l),")")
 end
     
 # constructor, destructor
-cms_new() = ccall((:cmsat_new, libcms), CMS, ())
-cms_free(p::CMS) = (ccall((:cmsat_free, libcms), Cvoid, (CMS,), p); p.ptr = C_NULL; nothing)
+cmsat_new() = ccall((:cmsat_new, libcms), CMS, ())
+cmsat_free(p::CMS) = (ccall((:cmsat_free, libcms), Cvoid, (CMS,), p); p.ptr = C_NULL; nothing)
 
 # add variables, clauses
-cms_nvars(p::CMS) = ccall((:cmsat_nvars, libcms), Cint, (CMS,), p)
-cms_add_clause(p::CMS, clause::Vector{Lit}) = Bool(ccall((:cmsat_add_clause, libcms), UInt8, (CMS,Ptr{Lit},Csize_t), p, clause, length(clause)))
-cms_add_xor_clause(p::CMS, vars::Vector{Cuint}, rhs::Bool) = Bool(ccall((:cmsat_add_xor_clause, libcms), UInt8, (CMS,Ptr{Cuint},Csize_t,Cuint), p, vars, length(vars), rhs))
-cms_new_vars(p::CMS, n::Int) = ccall((:cmsat_new_vars, libcms), Cvoid, (CMS,Csize_t), p, n)
+cmsat_nvars(p::CMS) = ccall((:cmsat_nvars, libcms), Cint, (CMS,), p)
+cmsat_add_clause(p::CMS, clause::Vector{CMSLit}) = Bool(ccall((:cmsat_add_clause, libcms), UInt8, (CMS,Ptr{CMSLit},Csize_t), p, clause, length(clause)))
+cmsat_add_xor_clause(p::CMS, vars::Vector{Cuint}, rhs::Bool) = Bool(ccall((:cmsat_add_xor_clause, libcms), UInt8, (CMS,Ptr{Cuint},Csize_t,Cuint), p, vars, length(vars), rhs))
+cmsat_new_vars(p::CMS, n::Int) = ccall((:cmsat_new_vars, libcms), Cvoid, (CMS,Csize_t), p, n)
 
 # solve
-cms_solve(p::CMS) = ccall((:cmsat_solve, libcms), UInt8, (CMS,), p)
-cms_solve_with_assumptions(p::CMS, assumptions::Vector{Lit}) = ccall((:cmsat_solve_with_assumptions, libcms), UInt8, (CMS,Ptr{Lit},Csize_t), p, assumptions, length(assumptions))
-cms_get_model(p::CMS) = unsafe_wrap(Array, ccall((:cmsat_get_model, libcms), Tuple{Ptr{UInt8},Csize_t}, (CMS,), p)...)
-cms_get_conflict(p::CMS) = unsafe_wrap(Array, ccall((:cmsat_get_conflict, libcms), Tuple{Ptr{Lit},Csize_t}, (CMS,), p)...)
+cmsat_solve(p::CMS) = ccall((:cmsat_solve, libcms), UInt8, (CMS,), p)
+cmsat_solve_with_assumptions(p::CMS, assumptions::Vector{CMSLit}) = ccall((:cmsat_solve_with_assumptions, libcms), UInt8, (CMS,Ptr{CMSLit},Csize_t), p, assumptions, length(assumptions))
+cmsat_get_model(p::CMS) = unsafe_wrap(Array, ccall((:cmsat_get_model, libcms), Tuple{Ptr{UInt8},Csize_t}, (CMS,), p)...)
+cmsat_get_conflict(p::CMS) = unsafe_wrap(Array, ccall((:cmsat_get_conflict, libcms), Tuple{Ptr{CMSLit},Csize_t}, (CMS,), p)...)
 
-cms_set_num_threads(p::CMS, n::Int) = ccall((:cmsat_set_num_threads, libcms), Cvoid, (CMS,Cuint), p, n)
+cmsat_print_stats(p::CMS) = ccall((:cmsat_print_stats, libcms), Cvoid, (CMS,), p)
+cmsat_set_num_threads(p::CMS, n::Int) = ccall((:cmsat_set_num_threads, libcms), Cvoid, (CMS,Cuint), p, n)
+cmsat_set_verbosity(p::CMS, n::Int) = ccall((:cmsat_set_verbosity, libcms), Cvoid, (CMS,Cuint), p, n)
+cmsat_set_default_polarity(p::CMS, polarity::Int) = ccall((:cmsat_set_default_polarity, libcms), Cvoid, (CMS,Cint), p, polarity)
+cmsat_set_polarity_auto(p::CMS) = ccall((:cmsat_set_polarity_auto, libcms), Cvoid, (CMS,), p)
+cmsat_set_no_simplify(p::CMS) = ccall((:cmsat_set_no_simplify, libcms), Cvoid, (CMS,), p)
+cmsat_set_no_simplify_at_startup(p::CMS) = ccall((:cmsat_set_no_simplify_at_startup, libcms), Cvoid, (CMS,), p)
+cmsat_set_no_equivalent_lit_replacement(p::CMS) = ccall((:cmsat_set_no_equivalent_lit_replacement, libcms), Cvoid, (CMS,), p)
+cmsat_set_no_bva(p::CMS) = ccall((:cmsat_set_no_bva, libcms), Cvoid, (CMS,), p)
+cmsat_set_no_bve(p::CMS) = ccall((:cmsat_set_no_bve, libcms), Cvoid, (CMS,), p)
+cmsat_set_up_for_scalmc(p::CMS) = ccall((:cmsat_set_up_for_scalmc, libcms), Cvoid, (CMS,), p)
+cmsat_set_yes_comphandler(p::CMS) = ccall((:cmsat_set_yes_comphandler, libcms), Cvoid, (CMS,), p)
+cmsat_simplify(p::CMS, assumptions::Vector{CMSLit}) = Bool(ccall((:cmsat_simplify, libcms), UInt8, (CMS,Ptr{CMSLit},Csize_t), p, clause, length(clause)))
+cmsat_set_max_time(p::CMS, max_time::Float64) = ccall((:cmsat_set_max_time, libcms), Cvoid, (CMS,Float64), p, max_time)
 
 ################################################################
-# higher-level interface
+# higher-level interface, 1-based
 
 """`new_vars(p::CMS, n::Int)`
 
 Add `n` new variables to the system `p`"""
-new_vars(p::CMS, n::Int) = cms_new_vars(p, n)
+new_vars(p::CMS, n::Int) = cmsat_new_vars(p, n)
 
 """`nvars(p::CMS)::Int`
 
 Return the number of variables in the system `p`"""
-nvars(p::CMS) = cms_nvars(p)
+nvars(p::CMS) = cmsat_nvars(p)
 
-"""`add_clause(p::CMS, clause::Vector{Union{Lit,Int}})::Bool`
+"""`add_clause(p::CMS, clause::Vector{Union{CMSLit,Integer}})::Bool`
 
 Add a clause to the system, namely the disjunction of the entries in `clause`.
-Direct terms are integers are numbered from 1, inverted terms are negative numbered from -1. They may also be specified as `Lit(variable,invert)`."""
-add_clause(p::CMS, clause::Vector{Lit}) = cms_add_clause(p,clause)
-function add_clause(p::CMS, clause)
-    c = convert(Array{Lit},clause)
+Direct terms are integers are numbered from 1, inverted terms are negative numbered from -1. They may also be specified as `CMSLit(variable,invert)`."""
+add_clause(p::CMS, clause::Vector{CMSLit}) = cmsat_add_clause(p,clause)
+function add_clause(p::CMS, clause::Vector{T}) where T <: Integer
+    c = convert(Vector{CMSLit},clause)
     m = -1
     for l in c
         m = max(m,var(l))
@@ -97,7 +113,7 @@ function add_clause(p::CMS, clause)
     if m ≥ nvars(p)
         new_vars(p, m+1-nvars(p))
     end
-    cms_add_clause(p,c)
+    cmsat_add_clause(p,c)
 end
 
 """`add_clauses(p::CMS, clauses::Vector)`
@@ -109,16 +125,17 @@ function add_clauses(p::CMS, clauses)
     end
 end
 
-"""`add_xor_clause(p::CMS, clause::Vector{Union{Lit,Int}}, rhs::Bool)::Bool`
+"""`add_xor_clause(p::CMS, clause::Vector{Union{CMSLit,Int}}, rhs::Bool)::Bool`
 
 Adds an XOR clause to `p`. This is a clause of the form `x_1 ⊻ x_2 ⊻ ... ⊻ x_n = rhs`."""
-add_xor_clause(p::CMS, clause::Vector{UInt32}, rhs::Bool = true) = cms_add_xor_clause(p, clause, rhs)
-function add_xor_clause(p::CMS, clause, rhs = true)
+add_xor_clause(p::CMS, clause::Vector{UInt32}, rhs::Bool = true) = cmsat_add_xor_clause(p, clause, rhs)
+function add_xor_clause(p::CMS, clause::Union{Vector{CMSLit},Vector{T}}, rhs = true) where T <: Integer
     cc = UInt32[]
     m = -1
     for c in clause
-        if isa(c, Lit)
+        if isa(c, CMSLit)
             x = var(c)
+            rhs ⊻= sign(c)
         elseif c>0
             x = c-1
         elseif c<0
@@ -133,7 +150,7 @@ function add_xor_clause(p::CMS, clause, rhs = true)
     if m ≥ nvars(p)
         new_vars(p, m+1-nvars(p))
     end    
-    cms_add_xor_clause(p, cc, rhs)
+    cmsat_add_xor_clause(p, cc, rhs)
 end
 
 """`add_xor_clauses(p::CMS, clauses::Vector)`
@@ -149,9 +166,9 @@ Cbool2Bool(v) = (v == 0x02 ? :undefined : v == 0x01 ? false : true)
 """`solve(p::CMS, assumptions=[])`
 
 returns whether `p` is solvable; possibly returns :undefined."""
-solve(p::CMS) = Cbool2Bool(cms_solve(p))
-function solve(p::CMS, assumptions)
-    c = convert(Array{Lit},assumptions)
+solve(p::CMS) = Cbool2Bool(cmsat_solve(p))
+function solve(p::CMS, assumptions::Vector{Union{CMSLit,Integer}})
+    c = convert(Vector{CMSLit},assumptions)
     m = -1
     for l in c
         m = max(m,var(l))
@@ -159,28 +176,28 @@ function solve(p::CMS, assumptions)
     if m ≥ nvars(p)
         new_vars(p, m+1-nvars(p))
     end
-    Cbool2Bool(cms_solve_with_assumptions(p,c))
+    Cbool2Bool(cmsat_solve_with_assumptions(p,c))
 end
 
 """`get_model(p::CMS)::Vector{Bool}`
 
 returns the variables in the current solution to `p`. This assumes that `solve(p)` was called and returned `true`."""
 function get_model(p::CMS)
-    map(Cbool2Bool,cms_get_model(p))
+    map(Cbool2Bool,cmsat_get_model(p))
 end
 
-"""`get_conflict(p::CMS)::Vector{Lit}`
+"""`get_conflict(p::CMS)::Vector{CMSLit}`
 
 returns the conflicting variables in the current solution to `p`. This assumes that `solve(p)` was called and returned `false`"""
 function get_conflict(p::CMS)
-    cms_get_conflict(p)
+    cmsat_get_conflict(p)
 end
 
 ################################################################
 # top-level interface, creating / destructing CMS object in one go.
 # the syntax is compatible with e.g. PicoSAT
 
-function create_problem(clauses, vars, num_threads)
+function create_problem(clauses, vars, verbose, num_threads)
     numvars = -1
     for c=clauses, l=c
         numvars = max(numvars,abs(l))
@@ -191,6 +208,7 @@ function create_problem(clauses, vars, num_threads)
     vars < numvars && error("There are more variables than specified")
 
     p = CMS(vars; num_threads = num_threads)
+    cmsat_set_verbosity(p, verbose)
     add_clauses(p, clauses)
     p
 end
@@ -198,7 +216,7 @@ end
 """
 `solve(clauses; vars::Integer=-1, verbose::Integer=0, proplimit::Integer=0, num_threads::Int = Sys.CPU_THREADS)`
    - `vars` - the number of variables (will be inferred if not specified)
-   - `verbose` - ignored
+   - `verbose` - verbosity
    - `proplimit` - ignored
    - `num_threads` - number of CPU threads to use
 Returns a solution if the problem is satisfiable.
@@ -206,7 +224,7 @@ Satisfiable solutions are represented as a vector of signed integers.
 If the problem is not satisfiable the method returns an `:unsatisfiable` symbol.
 ```julia
 julia> import CryptoMiniSat
-julia> cnf = Any[[1, -5, 4], [-1, 5, 3, 4], [-3, -4]];
+julia> cnf = [[1, -5, 4], [-1, 5, 3, 4], [-3, -4]];
 julia> CryptoMiniSat.solve(cnf)
 5-element Array{Int64,1}:
  -1
@@ -216,12 +234,12 @@ julia> CryptoMiniSat.solve(cnf)
  -5
 ```
 """
-function solve(clauses;
+function solve(clauses::Vector{Vector{T}};
                vars::Integer=-1,
                verbose::Integer=0,
                proplimit::Integer=0,
-               num_threads::Integer=Sys.CPU_THREADS)
-    p = create_problem(clauses, vars, num_threads)
+               num_threads::Integer=Sys.CPU_THREADS) where T <: Integer
+    p = create_problem(clauses, vars, verbose, num_threads)
     if solve(p)
         sol = get_model(p)
         return [sol[i] ? i : -i for i=1:length(sol)]
@@ -236,16 +254,15 @@ end
 """
 `itersolve(clauses; vars::Integer=-1, verbose::Integer=0, proplimit::Integer=0, num_threads::Integer=Sys.CPU_THREADS)`
    - `vars` - the number of variables
-   - `verbose` - ignored
+   - `verbose` - verbosity
    - `proplimit` - ignored
    - `num_threads` - number of CPU threads to use
 
 Returns an iterable object over all solutions.
-When a user-defined propagation limit is specified, the iterator may not produce all feasible solutions.
 
 ```julia
 julia> import CryptoMiniSat
-julia> cnf = Any[[1, -5, 4], [-1, 5, 3, 4], [-3, -4]];
+julia> cnf = [[1, -5, 4], [-1, 5, 3, 4], [-3, -4]];
 julia> collect(CryptoMiniSat.itersolve(cnf))
 18-element Array{Any,1}:
  [-1, -2, -3, -4, -5]
@@ -268,12 +285,12 @@ julia> collect(CryptoMiniSat.itersolve(cnf))
  [1, 2, 3, -4, -5]
 ```
 """
-function itersolve(clauses;
+function itersolve(clauses::Vector{Vector{T}};
                    vars::Integer=-1,
                    verbose::Integer=0,
                    proplimit::Integer=0,
-                   num_threads::Integer=Sys.CPU_THREADS)
-    p = create_problem(clauses, vars, num_threads)
+                   num_threads::Integer=Sys.CPU_THREADS) where T <: Integer
+    p = create_problem(clauses, vars, verbose, num_threads)
     return CMSIterator(p)
 end
 
@@ -291,4 +308,172 @@ function Base.iterate(it::CMSIterator, state=nothing)
 end
 
 IteratorSize(it::CMSIterator) = Base.SizeUnknown()
+
+################################################################
+# super-top level interface, in which the terms are arbitrary, and
+# coded into integers by a Dictionary
+
+struct Lit{T}
+    x::T
+    sign::Int
+end
+
+"""
+`Lit(x,inverted::Bool = false)`
+Creates a literal `x`
+"""
+Lit(x::T,inverted::Bool = false) where T = Lit{T}(x,inverted)
+
+"""
+`NotLit(x)`
+Creates a negated literal `x`; this is equivalent to `Lit(x,true)`
+"""
+NotLit(x::T) where T = Lit{T}(x,true)
+
+"""
+`XorLit(x)`
+Creates a literal `x`, to be used in an XOR clause
+"""
+XorLit(x::T) where T = Lit{T}(x,-2)
+
+"""
+`XNorLit(x)`
+Creates a negated literal `x`, to be used in an XOR clause
+"""
+XNorLit(x::T) where T = Lit{T}(x,-1)
+
+function Base.show(io::IO,x::Lit{T}) where T
+    isodd(x.sign) && print(io,"¬")
+    x.sign<0 && print(io,"⊻")
+    show(io,x.x)
+end
+
+function create_problem(clauses::Vector{Vector{Lit{T}}}, verbose, num_threads) where T
+    vardict = Dict{T,Int32}()
+    varlist = T[]
+
+    for c=clauses, l=c
+        haskey(vardict,l.x) && continue
+        push!(varlist,l.x)
+        vardict[l.x] = length(varlist)
+    end
+
+    p = CMS(length(varlist); num_threads = num_threads)
+    cmsat_set_verbosity(p, verbose)
+    
+    for c=clauses
+        is_xor = any(l->l.sign<0,c)
+        is_xor == all(l->l.sign<0,c) || error("Clause $c contains a mix of Xor and usual terms")
+        if is_xor
+            rhs = true
+            newc = UInt32[]
+            for l=c
+                push!(newc, vardict[l.x]-1)
+                rhs ⊻= Bool(l.sign&1)
+            end
+            add_xor_clause(p, newc, rhs)
+        else
+            newc = CMSLit[]
+            for l=c
+                push!(newc, CMSLit(vardict[l.x]-1,Bool(l.sign)))
+            end
+            add_clause(p, newc)
+        end
+    end
+    (p, vardict, varlist)
+end
+
+"""
+`solve(clauses; verbose::Integer=0, num_threads::Int = Sys.CPU_THREADS)`
+   - `verbose` - verbosity
+   - `num_threads` - number of CPU threads to use
+Returns a solution if the problem is satisfiable.
+Satisfiable solutions are represented as a dictionary.
+
+The first argument is a vector of clauses. Each clause is a vector of
+literals of, each encapsulated in `Lit(T)`, `NotLit(T)`, `XorLit(T)`
+or `XNorLit(T)` for some type `T`. Each clause containing XorLit and
+XNotLit terms is an XOR clause, and the number of satisfied clauses
+must have the same parity as the number of NotLit and XNorLit for the
+clause to be satified. Clauses containing Lit and NotLit terms are
+usual disjunctive clauses.
+
+If the problem is not satisfiable the method returns an `:unsatisfiable` symbol.
+```julia
+julia> import CryptoMiniSat
+julia> # coffee with cream, tea with milk
+julia> cnf = [[XorLit(:coffee),XorLit(:tea)],[NotLit(:coffee),Lit(:cream)],[NotLit(:tea),Lit(:milk)]]
+julia> CryptoMiniSat.solve(cnf)
+Dict{Symbol, Bool} with 3 entries:
+  :cream  => 0
+  :milk   => 1
+  :tea    => 1
+  :coffee => 0
+```
+"""
+function solve(clauses::Vector{Vector{Lit{T}}};
+               verbose::Integer=0,
+               num_threads::Integer=Sys.CPU_THREADS) where T
+    (p, vardict, varlist) = create_problem(clauses, verbose, num_threads)
+    if solve(p)
+        sol = get_model(p)
+        return Dict(varlist[i] => sol[i] for i=1:length(sol))
+    else
+        return :unsatisfiable
+    end
+end
+
+struct CMSDIterator{T}
+    p::CMS
+    d::Dict{T,UInt32}
+    l::Vector{T}
+end
+
+"""
+`itersolve(clauses; verbose::Integer=0, num_threads::Integer=Sys.CPU_THREADS)`
+   - `verbose` - verbosity
+   - `num_threads` - number of CPU threads to use
+
+The first argument is a vector of clauses. Each clause is a vector of
+literals of, each encapsulated in `Lit(T)`, `NotLit(T)`, `XorLit(T)`
+or `XNorLit(T)` for some type `T`. Each clause containing XorLit and
+XNotLit terms is an XOR clause, and the number of satisfied clauses
+must have the same parity as the number of NotLit and XNorLit for the
+clause to be satified. Clauses containing Lit and NotLit terms are
+usual disjunctive clauses.
+
+Returns an iterable object over all solutions.
+```julia
+julia> import CryptoMiniSat
+julia> # coffee with cream, tea with milk
+julia> cnf = [[XorLit(:coffee),XorLit(:tea)],[NotLit(:coffee),Lit(:cream)],[NotLit(:tea),Lit(:milk)]]
+julia> collect(CryptoMiniSat.itersolve(cnf))
+4-element Vector{Any}:
+ Dict{Symbol, Bool}(:cream => 0, :milk => 1, :tea => 1, :coffee => 0)
+ Dict{Symbol, Bool}(:cream => 1, :milk => 1, :tea => 1, :coffee => 0)
+ Dict{Symbol, Bool}(:cream => 1, :milk => 1, :tea => 0, :coffee => 1)
+ Dict{Symbol, Bool}(:cream => 1, :milk => 0, :tea => 0, :coffee => 1)
+```
+"""
+function itersolve(clauses::Vector{Vector{Lit{T}}};
+                   verbose::Integer=0,
+                   num_threads::Integer=Sys.CPU_THREADS) where T
+    (p, vardict, varlist) = create_problem(clauses, verbose, num_threads)
+    return CMSDIterator{T}(p, vardict, varlist)
+end
+
+function Base.iterate(it::CMSDIterator, state=nothing)
+    if solve(it.p)
+        v = get_model(it.p)
+
+        # add inverse sol for next iter
+        add_clause(it.p, [v[i] ? -i : i for i=1:length(v)])
+        return (Dict(it.l[i] => v[i] for i=1:length(v)), nothing)
+    else
+        return nothing
+    end
+end
+
+IteratorSize(it::CMSDIterator) = Base.SizeUnknown()
+
 end
